2b889c67010779724c44201fb90cf996ecca2567
rizaadi <rizaadi890@gmail.com>
2025-09-07 17:15:57 +0700

    feat: adjust bottom bar scaffold for navigation bar

--- app/src/main/java/com/zephysus/zest/component/scaffold/ZestSaffold.kt
+++ app/src/main/java/com/zephysus/zest/component/scaffold/ZestSaffold.kt
@@ -1,6 +1,10 @@
 package com.zephysus.zest.component.scaffold
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,7 +13,7 @@ import com.zephysus.zest.component.dialog.LoaderDialog
 
 @Composable
 fun ZestScaffold(
-    modifier: Modifier = Modifier, 
+    modifier: Modifier = Modifier,
     zestTopAppBar: @Composable () -> Unit = {},
     content: @Composable (PaddingValues) -> Unit,
     floatingActionButton: @Composable () -> Unit = {},
@@ -28,6 +32,10 @@ fun ZestScaffold(
         topBar = zestTopAppBar,
         content = content,
         floatingActionButton = floatingActionButton,
-        bottomBar = bottomBar
+        bottomBar = {
+            Box(modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBars)) {
+                bottomBar()
+            }
+        }
     )
 }
